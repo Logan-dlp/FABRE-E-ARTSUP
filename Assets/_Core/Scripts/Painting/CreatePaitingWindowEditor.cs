95a429468ce6f0c65d2b86f8e472594f9cd21120
Logan <pix.59700@gmail.com>
2024-11-27 14:14:31 +0100

    Create Tools for generate painting object

---
+++ Assets/_Core/Scripts/Painting/CreatePaitingWindowEditor.cs
@@ -0,0 +1,173 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreatePaitingWindowEditor : EditorWindow
+{
+    private PaintingList _paintingList;
+    private Sprite _currentSpritePainting;
+    private string _currentNamePainting;
+    private bool isButtonDisabled;
+
+    private const string _itemPath = "Assets/_Core/Painting/ScriptableObject"; 
+    
+    [MenuItem("Tools/Create Paiting")]
+    public static void ShowWindow()
+    {
+        GetWindow<CreatePaitingWindowEditor>("Create Paiting");
+    }
+
+    private void OnGUI()
+    {
+        _paintingList = (PaintingList)EditorGUILayout.ObjectField("Painting List :", _paintingList, 
+                                                        typeof(PaintingList), 
+                                                        false) as PaintingList;
+        
+        _currentNamePainting = EditorGUILayout.TextField("Name : ", _currentNamePainting);
+        
+        EditorGUILayout.BeginHorizontal();
+        _currentSpritePainting = (Sprite)EditorGUILayout.ObjectField("Painting Sprite :", _currentSpritePainting, 
+                                                                typeof(Sprite), 
+                                                                false) as Sprite;
+        EditorGUILayout.EndHorizontal();
+
+        
+        GUI.enabled = !string.IsNullOrEmpty(_currentNamePainting) && _currentSpritePainting != null && _paintingList != null;
+        if (GUILayout.Button("Create Paiting !"))
+        {
+            CreateCustomPaiting();
+        }
+        GUI.enabled = true;
+
+        if (_paintingList != null && _paintingList.paintingItemList.Count > 0)
+        {
+            DrawPaintingList();
+        }
+    }
+
+    private void CreateCustomPaiting()
+    {
+        PaintingItem asset = ScriptableObject.CreateInstance<PaintingItem>();
+        asset.PaintingSprite = _currentSpritePainting;
+        
+        string assetPath = AssetDatabase.GenerateUniqueAssetPath($"{_itemPath}/{_currentNamePainting}.asset");
+        AssetDatabase.CreateAsset(asset, assetPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        
+        Debug.Log($"{_currentNamePainting} as been created !\n{assetPath}");
+        
+        _paintingList.AddPaintingItem(asset);
+    }
+
+    private void DeleteCustomPaiting(PaintingItem asset)
+    {
+        string assetPath = AssetDatabase.GetAssetPath(asset);
+        if (string.IsNullOrEmpty(assetPath))
+        {
+            Debug.LogError("Asset does not exist !");
+        }
+        
+        if (EditorUtility.DisplayDialog(
+                "Delete asset",
+                $"Are you sure you want to delete this asset : {assetPath} ?",
+                "Yes",
+                "No"))
+        {
+            if (AssetDatabase.DeleteAsset(assetPath))
+            {
+                Debug.Log($"Asset as been deleted !\n{assetPath}");
+            }
+            else
+            {
+                Debug.LogError($"Unable to delete asset !\n{assetPath}");
+            }
+            
+            AssetDatabase.Refresh();
+        }
+        else
+        {
+            Debug.Log("Deletion canceled !");
+        }
+        
+        _paintingList.RemovePaintingItem(asset);
+    }
+    
+    private void DrawPaintingList()
+    {
+        float windowWidth = position.width;
+        float itemWidth = 80f;
+        float spaceBetweenItems = 10f;
+
+        int itemsPerRow = Mathf.FloorToInt((windowWidth - spaceBetweenItems) / (itemWidth + spaceBetweenItems));
+
+        float defaultHeight = itemWidth + 30f;
+        int totalRows = Mathf.CeilToInt(_paintingList.paintingItemList.Count / (float)itemsPerRow);
+        float calculatedHeight = totalRows * (itemWidth + spaceBetweenItems) + 10f;
+        float boxHeight = Mathf.Max(defaultHeight, calculatedHeight);
+
+        float boxPositionY = 150f;
+
+        Rect boxRect = new Rect(10, boxPositionY, windowWidth - 20, boxHeight);
+        GUI.Box(boxRect, string.Empty);
+        DrawPaintingGrid(boxRect, itemWidth, itemsPerRow);
+    }
+
+    private void DrawPaintingGrid(Rect boxRect, float itemWidth, int itemsPerRow)
+    {
+        GUILayout.BeginArea(boxRect);
+
+        int currentItem = 0;
+        List<PaintingItem> paintingToRemoveList = new List<PaintingItem>();
+
+        for (int i = 0; i < _paintingList.paintingItemList.Count; i++)
+        {
+            if (currentItem == 0)
+            {
+                GUILayout.BeginHorizontal();
+            }
+
+            PaintingItem prefab = _paintingList.paintingItemList[i];
+            Texture2D prefabTexture = AssetPreview.GetAssetPreview(prefab.PaintingSprite);
+
+            using (new GUILayout.VerticalScope(GUILayout.Width(itemWidth)))
+            {
+                using (new GUILayout.HorizontalScope())
+                {
+                    if (GUILayout.Button(prefabTexture, GUILayout.Width(itemWidth), GUILayout.Height(itemWidth)))
+                    {
+                        EditorGUIUtility.PingObject(prefab);
+                    }
+
+                    if (GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(20)))
+                    {
+                        paintingToRemoveList.Add(prefab);
+                    }
+                }
+            }
+
+            currentItem++;
+
+            if (currentItem >= itemsPerRow)
+            {
+                GUILayout.EndHorizontal();
+                currentItem = 0;
+            }
+        }
+
+        if (currentItem > 0)
+        {
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndArea();
+
+        foreach (PaintingItem prefab in paintingToRemoveList)
+        {
+            DeleteCustomPaiting(prefab);
+        }
+
+        Repaint();
+    }
+}
+
