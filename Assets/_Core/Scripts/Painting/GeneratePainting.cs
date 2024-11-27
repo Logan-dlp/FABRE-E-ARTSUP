ba3fc493b6393632d2ef8da27930d16ba4230b0f
Logan <pix.59700@gmail.com>
2024-11-27 14:13:49 +0100

    Generate random painting

---
+++ Assets/_Core/Scripts/Painting/GeneratePainting.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using FABRE.Camera;
+
+namespace FABRE.Painting
+{
+    public class GeneratePainting : MonoBehaviour
+    {
+        [SerializeField] private PaintingList _paintingList;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+        public void Generate()
+        {
+            if (_paintingList != null)
+            {
+                int randomIndex = Random.Range(0, _paintingList.paintingItemList.Count);
+                _spriteRenderer.sprite = _paintingList.paintingItemList[randomIndex].PaintingSprite;
+                CameraMovement.SetRandomPositionInSprite(_spriteRenderer);
+            }
+        }
+    }
+}
