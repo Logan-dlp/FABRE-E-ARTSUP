bf385dcf48e57d765530133e16b1745e5ebe2af1
Logan <pix.59700@gmail.com>
2024-12-06 22:27:29 +0100

    method breakdown

--- Assets/_Core/Scripts/Painting/Tools/CreatePaintingWindowEditor.cs
+++ Assets/_Core/Scripts/Painting/Tools/CreatePaintingWindowEditor.cs
@@ -1,104 +1,138 @@
+#if UNITY_EDITOR
+
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
-#if UNITY_EDITOR
 namespace FABRE.Painting.Tools
 {
     public class CreatePaintingWindowEditor : EditorWindow
     {
         private PaintingList _paintingList;
         private Sprite _currentSpritePainting;
+        private List<Vector2> _currentKeyPointsList;
+        
+        private Vector2 _newKeyPoint;
+        
+        private Vector2 _scrollPositionPaintingList;
+        private Vector2 _scrollPositionKeyPointList;
+        
         private string _currentNamePainting;
         private string _currentDescriptionPainting;
+        
+        private int _selectedKeyPointIndex = -1;
+        
         private bool isButtonDisabled;
     
-        private const string _itemPath = "Assets/_Core/Painting/ScriptableObject"; 
+        private const string _itemPath = "Assets/_Core/Painting/ScriptableObject";
         
         [MenuItem("Tools/Create Painting")]
         public static void ShowWindow()
         {
             GetWindow<CreatePaintingWindowEditor>("Create Painting");
         }
-    
-        private void OnGUI()
+
+        private void PaintingBasicInfoLayout()
         {
             _paintingList = (PaintingList)EditorGUILayout.ObjectField("Painting List :", _paintingList, 
-                                                            typeof(PaintingList), 
-                                                            false) as PaintingList;
+                typeof(PaintingList), 
+                false) as PaintingList;
             
             _currentNamePainting = EditorGUILayout.TextField("Name : ", _currentNamePainting);
             
             EditorGUILayout.BeginHorizontal();
             _currentSpritePainting = (Sprite)EditorGUILayout.ObjectField("Sprite :", _currentSpritePainting, 
-                                                                    typeof(Sprite), 
-                                                                    false) as Sprite;
+                typeof(Sprite), 
+                false) as Sprite;
             EditorGUILayout.EndHorizontal();
             
             _currentDescriptionPainting = EditorGUILayout.TextField("Description : ", _currentDescriptionPainting, GUILayout.Height(50));
-    
+        }
+
+        private void KeyPointLayout()
+        {
+            EditorGUILayout.BeginHorizontal();
+            _newKeyPoint = EditorGUILayout.Vector2Field("New KeyPoint :", _newKeyPoint);
+            if (GUILayout.Button("+"))
+            {
+                _currentKeyPointsList.Add(_newKeyPoint);
+                _newKeyPoint = Vector2.zero;
+            }
+            EditorGUILayout.EndHorizontal();
             
-            GUI.enabled = !string.IsNullOrEmpty(_currentNamePainting) && _currentSpritePainting != null && _paintingList != null && !string.IsNullOrEmpty(_currentDescriptionPainting);
+            EditorGUILayout.Space();
+            
+            _scrollPositionKeyPointList = EditorGUILayout.BeginScrollView(_scrollPositionKeyPointList, GUILayout.Height(_currentKeyPointsList.Count * 50));
+            for (int i = 0; i < _currentKeyPointsList.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+            
+                _currentKeyPointsList[i] = EditorGUILayout.Vector3Field($"Élément {i + 1}", _currentKeyPointsList[i]);
+            
+                if (GUILayout.Toggle(_selectedKeyPointIndex == i, "Sélectionner", "Button", GUILayout.Width(100)))
+                {
+                    _selectedKeyPointIndex = i;
+                }
+            
+                if (GUILayout.Button("X", GUILayout.Width(30)))
+                {
+                    _currentKeyPointsList.RemoveAt(i);
+                    if (_selectedKeyPointIndex == i)
+                    {
+                        _selectedKeyPointIndex = -1;
+                    }
+                    else if (_selectedKeyPointIndex > i)
+                    {
+                        _selectedKeyPointIndex--;
+                    }
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.Space();
+        
+            if (_selectedKeyPointIndex != -1 && _selectedKeyPointIndex < _currentKeyPointsList.Count)
+            {
+                GUILayout.Label($"Élément sélectionné : {_currentKeyPointsList[_selectedKeyPointIndex]}");
+            }
+            else
+            {
+                GUILayout.Label("Aucun élément sélectionné");
+            }
+        }
+
+        private void CreatePaintingLayout()
+        {
+            GUI.enabled = !string.IsNullOrEmpty(_currentNamePainting)
+                          && _currentSpritePainting != null 
+                          && _paintingList != null 
+                          && !string.IsNullOrEmpty(_currentDescriptionPainting);
             if (GUILayout.Button("Create Paiting !"))
             {
-                CreateCustomPainting();
+                PaintingInstance.Create(_paintingList, _itemPath,  _currentNamePainting, _currentSpritePainting, _currentDescriptionPainting);
+                
+                _currentNamePainting = string.Empty;
+                _currentSpritePainting = null;
+                _currentDescriptionPainting = string.Empty;
             }
             GUI.enabled = true;
+        }
+    
+        private void OnGUI()
+        {
+            PaintingBasicInfoLayout();
+    
+            KeyPointLayout();
+            
+            CreatePaintingLayout();
     
             if (_paintingList != null && _paintingList.paintingItemList.Count > 0)
             {
                 DrawPaintingList();
             }
-        }
-    
-        private void CreateCustomPainting()
-        {
-            PaintingItem asset = ScriptableObject.CreateInstance<PaintingItem>();
-            asset.PaintingName = _currentNamePainting;
-            asset.PaintingSprite = _currentSpritePainting;
-            asset.PaintingDescription = _currentDescriptionPainting;
-            
-            string assetPath = AssetDatabase.GenerateUniqueAssetPath($"{_itemPath}/{_currentNamePainting}.asset");
-            AssetDatabase.CreateAsset(asset, assetPath);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-            
-            Debug.Log($"{_currentNamePainting} as been created !\n{assetPath}");
-            
-            _paintingList.AddPaintingItem(asset);
-        }
-    
-        private void DeleteCustomPainting(PaintingItem asset)
-        {
-            string assetPath = AssetDatabase.GetAssetPath(asset);
-            if (string.IsNullOrEmpty(assetPath))
-            {
-                Debug.LogError("Asset does not exist !");
-            }
-            
-            if (EditorUtility.DisplayDialog(
-                    "Delete asset",
-                    $"Are you sure you want to delete this asset : {assetPath} ?",
-                    "Yes",
-                    "No"))
-            {
-                if (AssetDatabase.DeleteAsset(assetPath))
-                {
-                    Debug.Log($"Asset as been deleted !\n{assetPath}");
-                }
-                else
-                {
-                    Debug.LogError($"Unable to delete asset !\n{assetPath}");
-                }
-                
-                AssetDatabase.Refresh();
-            }
-            else
-            {
-                Debug.Log("Deletion canceled !");
-            }
-            
-            _paintingList.RemovePaintingItem(asset);
         }
         
         private void DrawPaintingList()
@@ -114,10 +148,9 @@ namespace FABRE.Painting.Tools
             float calculatedHeight = totalRows * (itemWidth + spaceBetweenItems) + 10f;
             float boxHeight = Mathf.Max(defaultHeight, calculatedHeight);
     
-            float boxPositionY = 200f;
+            float boxPositionY = 500;
     
             Rect boxRect = new Rect(10, boxPositionY, windowWidth - 20, boxHeight);
-            GUI.Box(boxRect, string.Empty);
             DrawPaintingGrid(boxRect, itemWidth, itemsPerRow);
         }
     
@@ -128,6 +161,7 @@ namespace FABRE.Painting.Tools
             int currentItem = 0;
             List<PaintingItem> paintingToRemoveList = new List<PaintingItem>();
     
+            _scrollPositionPaintingList = EditorGUILayout.BeginScrollView(_scrollPositionPaintingList, GUILayout.Height(200));
             for (int i = 0; i < _paintingList.paintingItemList.Count; i++)
             {
                 if (currentItem == 0)
@@ -137,7 +171,7 @@ namespace FABRE.Painting.Tools
     
                 PaintingItem prefab = _paintingList.paintingItemList[i];
                 Texture2D prefabTexture = AssetPreview.GetAssetPreview(prefab.PaintingSprite);
-    
+                
                 using (new GUILayout.VerticalScope(GUILayout.Width(itemWidth)))
                 {
                     using (new GUILayout.HorizontalScope())
@@ -162,6 +196,7 @@ namespace FABRE.Painting.Tools
                     currentItem = 0;
                 }
             }
+            EditorGUILayout.EndScrollView();
     
             if (currentItem > 0)
             {
@@ -172,11 +207,12 @@ namespace FABRE.Painting.Tools
     
             foreach (PaintingItem prefab in paintingToRemoveList)
             {
-                DeleteCustomPainting(prefab);
+                PaintingInstance.Delete(_paintingList, prefab);
             }
     
             Repaint();
         }
     }
 }
+
 #endif
